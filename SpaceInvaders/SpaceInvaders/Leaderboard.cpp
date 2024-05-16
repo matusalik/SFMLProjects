37bca1dc24b02bba380a3fb3b6ce1f8f67b84a8c
matusalik <mateuszsalik@gmail.com>
2024-05-17 00:57:31 +0200

    Reading files via filesystem not working, will fix it next time

--- SpaceInvaders/SpaceInvaders/Leaderboard.cpp
+++ SpaceInvaders/SpaceInvaders/Leaderboard.cpp
@@ -1,22 +1,22 @@
 #include "Leaderboard.h"
 Leaderboard::Leaderboard() {
 	std::filesystem::path directory;
-	std::ifstream directoriesFile("Data\\dir.txt");
-	if (directoriesFile.is_open()) {
+	std::ifstream directoryFile("Data/dir.txt");
+	if (directoryFile.is_open()) {
 		std::string line;
-		while (std::getline(directoriesFile, line)) {
-			std::cout << line << std::endl;
+		while (std::getline(directoryFile, line)) {
 			directory = line;
-			if (std::filesystem::exists(directory)) {
-				std::cout << "found directory!" << std::endl;
+			if (std::filesystem::exists(line)) {
+				break;
 			}
-			return;
 		}
+		directoryFile.close();
 	}
-	else {
-		std::cout << "Couldn't find directories file!" << std::endl;
-	}
-	/*if (file.is_open()) {
+	std::cout << directory << std::endl;
+	std::filesystem::path filepath = directory / "database.txt";
+	std::cout << filepath << std::endl;
+	std::ifstream file("filepath");
+	if (file.is_open()) {
 		std::string line;
 		while (std::getline(file, line)) {
 			std::istringstream iss(line);
@@ -35,7 +35,11 @@ Leaderboard::Leaderboard() {
 	for (auto i : this->database) {
 		std::cout << i.getNick() << " ";
 		std::cout << i.getScore() << std::endl;
-	}*/
+	}
+}
+std::filesystem::path Leaderboard::findFile(const std::string& name) {
+	std::filesystem::path mainpath;
+	return mainpath;
 }
 std::vector<Player>Leaderboard::getDatabase() {
 	return this->database;
--- SpaceInvaders/SpaceInvaders/Leaderboard.h
+++ SpaceInvaders/SpaceInvaders/Leaderboard.h
@@ -10,6 +10,7 @@ class Leaderboard
 {
 private:
 	std::vector<Player>database;
+	std::filesystem::path findFile(const std::string& name);
 public:
 	Leaderboard();
 	std::vector<Player>getDatabase();
