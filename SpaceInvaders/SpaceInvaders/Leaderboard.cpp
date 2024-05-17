88f5abf12f746f54784b1c051c53aba1a3b6ba2c
Mateusz Salik <mateuszsalik@gmail.com>
2024-05-17 20:33:04 +0200

    Rewritten the leaderboard constructor with <filesystem> to be more universal

--- SpaceInvaders/SpaceInvaders/Leaderboard.cpp
+++ SpaceInvaders/SpaceInvaders/Leaderboard.cpp
@@ -1,40 +1,30 @@
 #include "Leaderboard.h"
 Leaderboard::Leaderboard() {
-	//Using ifstream to serch through the dir.txt text file with possible directories of Data folder
-	//If you are a new user of this game you should add path to mentioned text file
-	std::filesystem::path directory;
-	std::ifstream directoryFile("Data/dir.txt");
-	if (directoryFile.is_open()) {
-		std::string line;
-		while (std::getline(directoryFile, line)) {
-			directory = line;
-			if (std::filesystem::exists(line)) {
-				std::filesystem::path filepath = directory / "database.txt";
-				std::ifstream file(filepath.string());
-				if (file.is_open()) {
-					std::string line;
-					while (std::getline(file, line)) {
-						std::istringstream iss(line);
-						std::string nick;
-						int score;
-						if (iss >> nick >> score) {
-							Player temp(nick);
-							temp.setScore(score);
-							this->database.push_back(temp);
-						}
-					}
-				}
-				else {
-					std::cout << "Couldn't find database file!" << std::endl;
+	std::filesystem::path directory = std::filesystem::current_path();
+	std::filesystem::path filepath = directory / "Data\\database.txt";
+	if (std::filesystem::exists(filepath)) {
+		std::ifstream file(filepath.string());
+		if (file.is_open()) {
+			std::string line;
+			while (std::getline(file, line)) {
+				std::istringstream iss(line);
+				std::string nick;
+				int score;
+				if (iss >> nick >> score) {
+					Player temp(nick);
+					temp.setScore(score);
+					this->database.push_back(temp);
 				}
 			}
 		}
-		directoryFile.close();
+		else {
+			std::cout << "Couldn't open the database file!" << std::endl;
+		}
 	}
 	else {
-		std::cout << "Couldn't find dir file!" << std::endl;
+		std::cout << "Couldn't find the database file!" << std::endl;
 	}
-	for (auto i : this->database) {
+	for (auto &i : this->database) {
 		std::cout << i.getNick() << " ";
 		std::cout << i.getScore() << std::endl;
 	}
